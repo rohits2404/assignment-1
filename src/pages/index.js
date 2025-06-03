5ab194451e30192dd2a20a6d4793386e6ecf51d8
Rohit Sharma <rohits8731@gmail.com>
2025-06-04 00:50:49 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import LandingScreen from "./LandingScreen";
+import LoginPage from "./LoginPage";
+import RegisterForm from "./RegisterForm";
+import AccountSettings from "./AccountSettings";
+
+export {
+    LandingScreen,
+    LoginPage,
+    RegisterForm,
+    AccountSettings
+}
